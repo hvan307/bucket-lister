9a3f6fc5eb494930676cb71610506ecd3eed907d
hvan307 <hongvan307@gmail.com>
2020-05-26 19:12:26 +0100

    user models, add not working

--- users/models.py
+++ users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Activity(models.Model):
     name = models.CharField(max_length=100)
-    description = models.CharField
+    description = models.CharField(max_length=500)
 
     def __str__(self):
         return f'{self.name} {self.description}'
@@ -16,13 +16,8 @@ class PlaceToVisit(models.Model):
         return f'{self.name} {self.city} {self.country}'
 
 class User(models.Model):
-    username = models.CharField(max_length=50)
-    email = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
-    pass_confirm = models.CharField(max_length=50)
-    image = models.CharField(max_length=200, blank=True)
     activities = models.ForeignKey(Activity, related_name='users', on_delete=models.CASCADE, blank=True)
-    places_to_visit = models.ForeignKey(PlaceToVisit, related_name='users', on_delete=models.CASCADE)
+    places_to_visit = models.ForeignKey(PlaceToVisit, related_name='users', on_delete=models.CASCADE, blank=True)
 
     def __str__(self):
         return f'{self.username} {self.email} {self.image} {self.activities} {self.places_to_visit}'
