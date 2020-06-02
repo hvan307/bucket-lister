6c94a5be3c3190d9ed35a0a7c50a0e71f0aee341
hvan307 <hongvan307@gmail.com>
2020-06-02 23:18:42 +0100

    add in admin not working, review models

--- users/models.py
+++ users/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+# from django.contrib.auth.models import AbstractUser
+
 
 class Activity(models.Model):
     name = models.CharField(max_length=100)
@@ -20,4 +22,4 @@ class User(models.Model):
     places_to_visit = models.ForeignKey(PlaceToVisit, related_name='users', on_delete=models.CASCADE, blank=True)
 
     def __str__(self):
-        return f'{self.username} {self.email} {self.image} {self.activities} {self.places_to_visit}'
+        return self.username
